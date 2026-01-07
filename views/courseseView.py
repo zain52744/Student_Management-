30c0493b78fd3bdc014d59698bbf0b783e2e1c91
zain52744 <mohammadzain52744@gmail.com>
2026-01-07 19:05:29 +0500

    this is my student management system bt using falsk api

--- views/courseseView.py
+++ views/courseseView.py
@@ -58,46 +58,9 @@ def delete_teacher(teach_id):
    return teacher_controller.delete_teach_by_id(teach_id)
 
 #FUNCTIONALITY OF SUBJECTS TABLE
-@course_app.route("/addsubject",methods = ['POST'])
-def add_subjects():
-   return subject_controller.add_course()
 
-@course_app.route("/getsubjects",methods = ['GET'])
-def get_all_subjects():
-   return subject_controller.get_subjects()
-
-@course_app.route("/getsubject/<int:subj_id>",methods = ['GET'])
-def get_subject_by_id(subj_id):
-   return subject_controller.get_subj_by_id(subj_id)
-
-@course_app.route("/updatesubject/<int:subj_id>",methods = ['PUT'])
-def update_subject_by_id(subj_id):
-   return subject_controller.update_subj_by_id(subj_id)
-
-@course_app.route("/deletesubject/<int:subj_id>",methods = ['DELETE'])
-def delete_subject_by_id(subj_id):
-   return subject_controller.delete_subj_by_id(subj_id)
 
 #FUNCTIONALITY OF SCORE TABLE
-@course_app.route("/addscores", methods = ['POST'])
-def add_scores():
-   return score_controller.add_scores()
-
-@course_app.route("/getscores",methods = ['GET'])
-def get_all_scores():
-   return score_controller.get_scores()
-
-@course_app.route("/getscore/<int:sco_id>",methods = ['GET'])
-def get_score_by_id(sco_id):
-   return score_controller.get_sco_by_id(sco_id)
-
-@course_app.route("/updatescore/<int:sco_id>",methods = ['PUT'])
-def update_score_by_id(sco_id):
-   return score_controller.update_sco_by_id(sco_id)
-
-@course_app.route("/deletescore/<int:sco_id>",methods = ['DELETE'])
-def delete_score_by_id(sco_id):
-   return score_controller.delete_sco_by_id(sco_id)
 
 if __name__ == '__main__':  
    course_app.run(debug=True)
